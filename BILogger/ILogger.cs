743bc3170db59e970fb2eeba96ace51971f572a4
akshaykumar684 <42766424+akshaykumar684@users.noreply.github.com>
2020-07-20 02:58:13 +0530

    Updated Material design package
    Updated Material design package

--- BILogger/ILogger.cs
+++ BILogger/ILogger.cs
@@ -5,17 +5,11 @@ namespace BILogger
 {
     public interface ILogger
     {
-        string LogFile { get; }
-
-        void checkDirectory();
-
+      
         void Error(Exception errorData, [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0);
 
 
         void Message(string messageData, [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0);
-
-
-        void Log(string str);
 
     }
 }
